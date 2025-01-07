0e6cd7fbeb12f6130a688a1cddfd0e073fee9caf
QuantumFD <contact@quantumfd.com>
2025-01-08 00:19:32 +0100

    step1 - integer

--- section03/09-dataTypes.f90
+++ section03/09-dataTypes.f90
@@ -1,6 +1,9 @@
 program Integer
-    implicit none
+    ! implicit none
     ! max value 2^(8*1-1=7)-1 -> 127 [100] => 10^2 
     integer(kind=1) :: small_num = 127
-    print *, small_num, " "
+    integer(kind=2) :: medium_num = 32767
+    integer(kind=4) :: big_num = 2147483647
+    integer(kind=8) :: huge_num = 9223372036854775807_8
+    print *, small_num, " ", medium_num, " ", big_num, " ", huge_num, " "
 end program Integer
