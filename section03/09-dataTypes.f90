a17797aa90afc74b290a4b04a99a1badfb96e8d6
QuantumFD <contact@quantumfd.com>
2025-01-07 23:03:47 +0100

    step 1

--- section03/09-dataTypes.f90
+++ section03/09-dataTypes.f90
@@ -1,11 +1,6 @@
 program Integer
     implicit none
-    integer :: a, b, c
-
-    a = 10
-    b = 20
-    c = a + b
-
-    print *, 'Sum of a and b is ', c
-
+    ! max value 2^(8*1-1=7)-1 -> 127 [100] => 10^2 
+    integer(kind=1) :: small_num = 127
+    print *, small_num, " "
 end program Integer
